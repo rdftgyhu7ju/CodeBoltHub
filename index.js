2b1bd3339cb50ec9d90828751df069eb2d826838
rdftgyhu7ju <gs1katedead@hotmail.com>
2024-05-07 23:41:01 +0700

    test commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else right = mid;
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
   }
-  return nums[left];
+  return count.every((c) => c === 0);
 }
