9eba9256241523c3ca19dcb2c1d1bf7ecf3b4589
rdftgyhu7ju <gs1katedead@hotmail.com>
2024-05-13 06:23:31 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const colors = ["red", "green", "blue"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  let min = Infinity;
+  if (root.left) min = Math.min(min, minDepth(root.left));
+  if (root.right) min = Math.min(min, minDepth(root.right));
+  return min + 1;
+}
