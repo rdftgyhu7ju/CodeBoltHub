83330b20a1630140a8bb50a5973b255f4fc2037d
rdftgyhu7ju <gs1katedead@hotmail.com>
2024-05-13 13:09:17 +0700

    start

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function minDepth(root) {
   if (!root) return 0;
-  if (!root.left && !root.right) return 1;
-  let min = Infinity;
-  if (root.left) min = Math.min(min, minDepth(root.left));
-  if (root.right) min = Math.min(min, minDepth(root.right));
-  return min + 1;
+  const queue = [root];
+  let depth = 1;
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (!node.left && !node.right) return depth;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    depth++;
+  }
+  return depth;
 }
